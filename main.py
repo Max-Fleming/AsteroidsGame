970be5fe1818277bfc68b953887ac0ffd3da91bd
Max Fleming <mtfleming94@gmail.com>
2025-04-01 14:29:41 -0700

    Created player model that rotates using /'a/' and /'d/' keys

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
-from constants import *  # noqa: F403
+from constants import *
+from player import Player  # noqa: F403
 
 
 def main():
     pygame.init()
+    # Creating screen using our constant variables
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    game_clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        # this variable will not only store the total game time that has passed, it will also keep our FPS at 60
+        game_time = game_clock.tick(60)
+        dt = game_time / 1000
 
 
 if __name__ == "__main__":
